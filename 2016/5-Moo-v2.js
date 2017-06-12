dd50c0e6a943bebb7a35530ced0b96a3b6e32eca
Hobgoblin101 <aj@bilby.net>
2017-06-12 14:46:48 +1000

    Attempted 2016-5 again

--- 2016/5-Moo-v2.js
+++ 2016/5-Moo-v2.js
@@ -45,25 +45,114 @@ function Encode(g){
   return r;
 }
 
-class Candidates{
-  constructor(){
-    this.options = [];
 
-    for (let i=0; i<=9999; i++){
-      this.options.push(i);
+
+class Rule{
+  constructor(number, statement){
+    this.statement = statement;   //example: BBC
+    this.number = number;         //example: 0431
+    this.b = 0;
+    this.c = 0;
+
+    for (let i=0; i<this.statement.length; i++){
+      if (this.statement[i] == "B"){
+        this.b += 1;
+      }else if (this.statement[i] == "C"){
+        this.c += 1;
+      }
     }
   }
 
-  get best(){
-    var i = Math.ceil(this.options.length/2); //Round up because we want the larger option, and they are in order
-    return this.options[i];
+  match(number){
+    var b = 0;
+    var c = 0;
+
+    var checked = [];
+    for (let i=0; i<this.number.length; i++){
+      if (number[i] === this.number[i]){
+        b =+ 1;
+        continue;
+      }
+
+      for (let j=0; j<this.number.length; j++){
+        if (checked[j]){
+          continue;
+        }
+
+        if (this.number[j] === number[i]){
+          checked[j] = true;
+          c += 1;
+          break; //found a place
+        }
+      }
+    }
+
+    return {b, c};
   }
 
-  match(number, rule){
-    
+  fits(number){
+    var m = this.match(number);
+    return m.b == this.b && m.c == this.c;
+  }
+}
+class Candidates{
+  constructor(){
+    this.options = [];
+    for (let i=0; i<=9999; i++){
+      var num = i.toString();
+      while (num.length<4){ //Fix length
+        num = '0'+num;
+      }
+
+      this.options.push(num);
+    }
+  }
+
+  constrain(rule){
+    for (let i=0; i<this.options.length; i++){
+      if (rule.fits(this.options[i]) !== true){
+        this.options.splice(i, 1);
+        i -= 1;
+      }
+    }
+
+    return this;
+  }
+
+  get best(){
+    //Ceil because we want the larger option and they are in order smallest - largest
+    return this.options[Math.ceil(this.options.length / 2)];
   }
 }
 
+
+
+var anwser = new Rule('9153', 'BBBB');
+
+// var number = Encode('0431');
 var game = new Candidates();
 
-console.log(game.best)
+for (var i=0; i<1000; i++){
+  var guessN = game.best;                        //Guess Number
+
+
+  // var guessR = Matcher(number, Encode(guessN));  //Guess Result
+  //CUSTOM
+  var guessR = '';
+  var r = anwser.match(guessN);
+  for (let i=0; i<r.b; i++){
+    guessR += 'B';
+  }
+  for (let i=0; i<r.c; i++){
+    guessR += 'C';
+  }
+
+  if (guessR === "BBBB"){
+    break;
+  }
+  game.constrain(new Rule(guessN, guessR));
+
+  console.log(i, guessN, r, guessR, game.options.length);
+}
+
+console.log(i, guessN, r, guessR, game.options.length);
